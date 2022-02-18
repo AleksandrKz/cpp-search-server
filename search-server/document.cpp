1af6d5219ca210944f5c3bc4b68c65a8651c85a5
Aleksandr Kuzyk <ya.a-leksandr@yandex.ru>
2022-02-18 14:36:34 +0300

    error correction

--- search-server/document.cpp
+++ search-server/document.cpp
@@ -4,6 +4,12 @@
 
 using namespace std::string_literals;
 
+Document::Document(int id, double relevance, int rating)
+        : id(id)
+        , relevance(relevance)
+        , rating(rating) {
+    }
+
 std::ostream& operator<<(std::ostream& out, const Document& document) {
     out << "{ "s
         << "document_id = "s << document.id << ", "s
