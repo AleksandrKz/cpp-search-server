8011964c7e71f55faa08848521ae0b9a9d1cc9e8
Aleksandr Kuzyk <ya.a-leksandr@yandex.ru>
2022-03-04 00:25:38 +0300

    error correction

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -7,23 +7,23 @@
 #include <map>
 
 void RemoveDuplicates(SearchServer& search_server){
-    std::vector<int> candidate;
-    std::set<std::set<std::string>> vectors;
+    std::vector<int> candidates_to_delete;
+    std::set<std::set<std::string>> all_documents;
 
     for (auto it = search_server.begin(); it != search_server.end(); ++it) {
-        std::map<std::string, double> move = search_server.GetWordFrequencies(*it);
-        std::set<std::string> move_str;
-        for (auto i : move){
-            move_str.insert(i.first);
+        std::map<std::string, double> current_document = search_server.GetWordFrequencies(*it);
+        std::set<std::string> document_words;
+        for (const auto& i : current_document){
+            document_words.insert(i.first);
         }
-        if (vectors.count(move_str)) {
-            candidate.push_back(*it);
+        if (all_documents.count(document_words)) {
+            candidates_to_delete.push_back(*it);
         } else {
-            vectors.insert(move_str);
+            all_documents.insert(document_words);
         }
     }
     
-    for(const int id : candidate){
+    for(const int id : candidates_to_delete){
         search_server.RemoveDocument(id);
        std::cout << "Found duplicate document id " << id << std::endl;
     }
