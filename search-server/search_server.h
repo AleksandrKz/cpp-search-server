d3603baf44efaa5fc60ab53ccf14894732ea5362
Aleksandr Kuzyk <ya.a-leksandr@yandex.ru>
2022-03-14 18:23:37 +0300

    correction

--- search-server/search_server.h
+++ search-server/search_server.h
@@ -74,7 +74,7 @@ private:
     std::map<std::string, std::map<int, double>> word_to_document_freqs_;
     std::map<int, DocumentData> documents_;
     std::set<int> document_ids_;
-    std::map<int, std::map<std::string, double>> all_documents_;
+    std::map<int, std::map<std::string, double>> document_to_word_freqs_;
     
     bool IsStopWord(const std::string& word) const;
     
